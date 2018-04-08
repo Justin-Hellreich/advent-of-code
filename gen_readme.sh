79bb0aa8947a94484a6eea07e00ba8e8f77a3c94
Justin Hellreich <jdhellre@uwaterloo.ca>
2018-04-08 16:33:11 -0400

    rename directories to preserve sorting order

--- gen_readme.sh
+++ gen_readme.sh
@@ -2,6 +2,6 @@
 
 for dir in 20*; do
   for question in $dir/*; do
-    echo "http://adventofcode.com/$dir/day/${question##*/}" > $question/README.md
+    echo "http://adventofcode.com/$dir/day/$((10#${question##*/}))" > $question/README.md
   done
 done
