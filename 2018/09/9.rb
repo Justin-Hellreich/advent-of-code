a334f0275b15b6c4a1d029e787b5132861d1f822
Justin Hellreich <jdhellre@uwaterloo.ca>
2018-12-17 12:53:44 -0800

    Re-factor to use a circular doubly linked list.

--- 2018/09/9.rb
+++ 2018/09/9.rb
@@ -1,51 +1,38 @@
-class CircularArray
-  attr_accessor :array
+class Node
+  attr_accessor :next, :prev, :val
 
-  def initialize(array)
-    @array = array
-  end
-
-  def at(i)
-    array.at index i
-  end
-
-  def insert(n, at:)
-    i = index(at)
-    @array.insert(i, n)
-    i
-  end
-
-  def delete(at: )
-    @array.delete_at(at % (array.size - 1))
-  end
-
-  private
-
-  def index(i)
-    i % array.size
+  def initialize(val)
+    @next = nil
+    @prev = nil
+    @val = val
   end
 end
 
 class Game
-  def initialize
+  def initialize(times: 1)
     @players, @last_marble = parse_file
+    @last_marble = @last_marble * times
   end
 
   def play
-    circle = CircularArray.new [0, 1]
+    current_marble = mk_default_double_dir_circular_linked_list.next
     turn = 2
-    current_marble_index = 1
     scores = Array.new(@players) { 0 }
 
     while turn < @last_marble
-      puts turn if (turn % 10000 == 0)
-      #puts "#{turn} #{current_marble_index} #{circle.array.inspect}"
-      if turn % 23 != 0
-        current_marble_index = circle.insert(turn, at: current_marble_index + 2)
+      if turn % 23 != 0 # insert after next node
+        after_new_node = current_marble.next.next
+        current_marble.next.next = Node.new turn
+        current_marble.next.next.prev = current_marble.next
+        current_marble.next.next.next = after_new_node
+        current_marble.next.next.next.prev = current_marble.next.next
+        current_marble = current_marble.next.next
       else
         scores[turn % @players] += turn
-        current_marble_index -= 7
-        scores[turn % @players] += circle.delete(at: current_marble_index)
+        current_marble = current_marble.prev.prev.prev.prev.prev.prev.prev
+        scores[turn % @players] += current_marble.val
+        current_marble = current_marble.next
+        current_marble.prev.prev.next = current_marble
       end
 
       turn += 1
@@ -56,10 +43,35 @@ class Game
 
   private
 
+  def mk_default_double_dir_circular_linked_list
+    el1 = Node.new 0
+    el2 = Node.new 1
+    el1.next = el2
+    el2.next = el1
+    el2.prev = el1
+    el1.prev = el2
+
+    el1
+  end
+
+  def print(node)
+    root = node.val
+    nums = [root]
+
+    node = node.next
+    while (node.val != root)
+      nums << node.val
+      node = node.next
+    end
+
+    puts nums.inspect
+  end
+
   REGEX = /(\d*).*\s(\d*)\s.*/
   def parse_file
     REGEX.match(File.read('data.txt'))[1..2].map(&:to_i)
   end
 end
 
-puts Game.new.play.inspect
+puts Game.new.play
+puts Game.new(times: 100).play
